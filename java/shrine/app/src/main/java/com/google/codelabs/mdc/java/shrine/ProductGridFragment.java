f170d2dfc38b8791ffa70caca7d90376c5dfec18
Danish <Danish.parvez19@gmail.com>
2019-01-01 00:49:31 +0530

    Implements Recycler View

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.codelabs.mdc.java.shrine.network.ProductEntry;
 
 public class ProductGridFragment extends Fragment {
 
@@ -27,6 +31,18 @@ public class ProductGridFragment extends Fragment {
 
         // Set up the toolbar
         setUpToolbar(view);
+
+        // Set up the RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2, GridLayoutManager.VERTICAL, false));
+        ProductCardRecyclerViewAdapter adapter = new ProductCardRecyclerViewAdapter(
+                ProductEntry.initProductEntryList(getResources()));
+        recyclerView.setAdapter(adapter);
+        int largePadding = getResources().getDimensionPixelSize(R.dimen.shr_product_grid_spacing);
+        int smallPadding = getResources().getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small);
+        recyclerView.addItemDecoration(new ProductGridItemDecoration(largePadding, smallPadding));
+
         return view;
     }
 
